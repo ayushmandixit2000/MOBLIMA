6f20cf7deeeaa1307665477b6b2cbaac25a52f91
ayushmandixit2000 <ayushmandixit2000@gmail.com>
2022-10-22 23:23:08 +0800

    added customer panel skeleton

--- customerpanel.java
+++ customerpanel.java
@@ -1,3 +1,58 @@
+import java.util.Scanner;
+
 public class customerpanel {
-    
+    userclass user = new userclass();
+    moviesclass movie = new moviesclass();
+    cineplexclass cineplex = new cineplex();
+
+
+    public void viewapp(){
+        System.out.println("Welcome to MOBLIMA");
+        Scanner scc = new Scanner(System.in);
+        loop: while(true){
+            System.out.println("Please select your desired action (key in the coresponding number)");
+            System.out.println("1: Search/List movie");
+            System.out.println("2: View movie details - including reviews and ratings");
+            System.out.println("3: Check seat availability and selections of seat/s");
+            System.out.println("4: Book and purshcase ticket");
+            System.out.println("5: View booking history");
+            System.out.println("6: List the Top 5 ranking by ticket salers OR by overall reviewers' ratings");
+            System.out.println("7: Quit");
+
+            int option = scc.nextInt();
+
+            switch(option){
+                case 1:
+                    System.out.println("Please select your desired action");
+                    System.out.println("1: List movies currently showing");
+                    System.out.println("2: Search a specific movie");
+                    int option1 = scc.nextInt();
+                    if(option1 == 1){
+                        movie.list();
+                    }
+                    else if(option == 2){
+                        movie.search();
+                    }
+                    break;
+
+                case 2:
+                    movie.viewdetails();
+
+                case 3:
+                    cineplex.seats();
+
+                case 4:
+                    user.booktickets();
+
+                case 5:
+                    user.bookinghistory();
+
+                case 6:
+                    movie.rank();
+                case 7:
+                    System.out.println("Exiting...");
+                    break loop;
+            }
+        }
+    }
 }
