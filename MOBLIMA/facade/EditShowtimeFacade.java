53965fadb5b9c6ea7860c8072b6c0af174940e61
lameo125 <marcus.hooi@gmail.com>
2022-11-10 23:00:57 +0800

    Update EditShowtimeFacade.java

--- MOBLIMA/facade/EditShowtimeFacade.java
+++ MOBLIMA/facade/EditShowtimeFacade.java
@@ -1,63 +1,25 @@
 package MOBLIMA.facade;
 
 import java.io.IOException;
-import java.time.LocalTime;
-import java.util.Scanner;
 
-import MOBLIMA.listingInterface.*;
-import MOBLIMA.remove.removeShowtime;
-import MOBLIMA.create.createShowtime;
+import MOBLIMA.action.adminMovieAction;
 
-public class EditShowtimeFacade {
-
-    public static String cineplex;
-    public static int cinema;
-    public static String cinemaCode;
-
-    public static void main(String[] args) throws IOException {
-
-        CinemaListing cnml = new CinemaListing();
-        ShowtimeListing sl = new ShowtimeListing();
-        CineplexListing cpxl = new CineplexListing();
-        adminShowtimeAction action = new adminShowtimeAction();
-        
-        // Cineplex Listing
-        cpxl.displayListing();
-        cineplex = cpxl.getListing();
-
-        // Cinema Listing
-        cnml.displayListing();
-        cinema = cnml.getListing();
-
-        int cineplexlength = cineplex.length();
-        // System.out.println(cineplex.charAt(cineplexlength-1));
-
-        // cinemaCode
-        // First + last letter of cineplex + cinema number
-        String cinemaCode = String.valueOf(cineplex.charAt(0)) + String.valueOf(cineplex.charAt(cineplexlength-1)) + String.valueOf(cinema);
-        System.out.println("Cinema Code: " + cinemaCode);
-
-        // Showitme Listing
-        sl.populate(cineplex, cinema, cinemaCode);
-        sl.displayListing();
-
-        // Ask use what would they like to do?
-        action.displayListing();
-        Scanner sc = new Scanner(System.in);
-        int todo = sc.nextInt();
-
-        switch(todo){
-            case 0: // Create Showtime
-            createShowtime.create(cineplex, cinema, cinemaCode);
-
-            case 1: // Update Showtime
-            int updateShowtimeIndex = sc.nextInt();
-            
-
-            case 2: // Remove Showtime
-            removeShowtime.remove(cineplex, cinema, cinemaCode);
-
+public class editShowtimeFacade {
+    public void run() throws IOException {
+        adminMovieAction ama = new adminMovieAction();
+        ama.displayChoices();
+        int action = ama.getChoice();
+        switch (action) {
+            case 1:
+                System.out.println("Create Movie Showtimes");
+                break;
+            case 2:
+                System.out.println("Update Movie Showtimes");
+                break;
+            case 3:
+                System.out.println("Remove Movie Showtimes");
+                break;
         }
-        
     }
+
 }
