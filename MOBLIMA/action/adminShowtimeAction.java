5749b85acd8baaff04125741227c9c92f1b91f8e
summitbajaj <95735368+summitbajaj@users.noreply.github.com>
2022-11-11 21:38:54 +0800

    error handling

--- MOBLIMA/action/adminShowtimeAction.java
+++ MOBLIMA/action/adminShowtimeAction.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class adminShowtimeAction implements action {
     public void displayChoices() {
         System.out.println("Please select your desired action (key in the coresponding number)");
-        System.out.println("1: Create Movie Listing");
-        System.out.println("2: Update Movie Listing");
-        System.out.println("3: Remove Movie Listing");
+        System.out.println("1: Create Movie Showtime");
+        System.out.println("2: Update Movie Showtime");
+        System.out.println("3: Remove Movie Showtime");
         System.out.println("4: Quit");
     }
 
@@ -16,12 +16,29 @@ public class adminShowtimeAction implements action {
         Scanner sc = new Scanner(System.in);
         boolean loop = true;
         int choice = 0;
-        while (loop) {
-            choice = sc.nextInt();
-            if (choice <= 5 && choice > 0) {
-                loop = false;
+        boolean flag;
+         // to prevent users from keying in data that is not string 
+         do{
+            try{
+                choice = sc.nextInt();
+                flag = false;
+                if (choice < 1 || choice > 4) {
+                    System.out.println("Invalid option. Please key in valid numbers");
+                    flag = true;
+                }
+                else{
+                    flag = false;
+                }
+            }
+            catch(Exception e){
+                System.out.println("Inavlid input. Please enter intergers only... "+e);
+                sc.nextLine();
+                flag = true;
             }
         }
+        while(flag);
+        sc.nextLine();
+        
         return choice;
 
     }
--- MOBLIMA/listing/CinemaListing.java
+++ MOBLIMA/listing/CinemaListing.java
@@ -18,14 +18,37 @@ public class CinemaListing implements Listing{
     }
 
     public int getListing(){
-        System.out.println("Enter the Cinema number you'd like to view: ");
-
         Scanner sc = new Scanner(System.in);
-        int cinemaNumber = sc.nextInt();
+        int cinemaNumber = -1;
+        boolean flag = false;
+
+           // to prevent users from keying in data that is not string 
+           do{
+            try{
+                System.out.println("Enter the Cinema number you'd like to view: ");
+                cinemaNumber = sc.nextInt();
+                flag = false;
+                if (cinemaNumber < 1 || cinemaNumber > 3) {
+                    System.out.println("Invalid option. Please key in valid numbers");
+                    flag = true;
+                }
+                else{
+                    flag = false;
+                }
+            }
+            catch(Exception e){
+                System.out.println("Inavlid input. Please enter intergers only... "+e);
+                sc.nextLine();
+                flag = true;
+            }
+        }
+        while(flag);
+        sc.nextLine();
+        
         System.out.println("");
         return cinemaNumber;
-
     }
+    
     
     public static void main(String[] args) throws IOException{
          CinemaListing cl = new CinemaListing();
