a387444db273f54870a18de80c00eff06b5ed5d0
summitbajaj <95735368+summitbajaj@users.noreply.github.com>
2022-11-11 21:23:36 +0800

    Cineplex Listing error handling

--- MOBLIMA/listing/CineplexListing.java
+++ MOBLIMA/listing/CineplexListing.java
@@ -18,12 +18,33 @@ public class CineplexListing implements Listing {
                 + "2 - Orchard\n"
                 + "3 - Ang Mo Kio\n");
     }
-
     public String getListing() {
         Scanner sc = new Scanner(System.in);
+        int cineplexchoice = -1;
+        boolean flag = false;
 
-        System.out.println("Please enter the cineplex you'd like to edit");
-        int cineplexchoice = sc.nextInt();
+           // to prevent users from keying in data that is not string 
+           do{
+            try{
+                System.out.println("Please enter the cineplex you'd like to edit");
+                cineplexchoice = sc.nextInt();
+                flag = false;
+                if (cineplexchoice < 0 || cineplexchoice > 3) {
+                    System.out.println("Invalid option. Please key in valid numbers");
+                    flag = true;
+                }
+                else{
+                    flag = false;
+                }
+            }
+            catch(Exception e){
+                System.out.println("Inavlid input. Please enter intergers only... "+e);
+                sc.nextLine();
+                flag = true;
+            }
+        }
+        while(flag);
+        sc.nextLine();
 
         switch (cineplexchoice) {
             case 0:
