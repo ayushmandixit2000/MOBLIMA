ffb24b27f8f6270dae7a7f0ac7245ee79ec10f6a
summitbajaj <95735368+summitbajaj@users.noreply.github.com>
2022-11-13 19:43:01 +0800

    javadoc

---
+++ MOBLIMA/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"MOBLIMA"},{"l":"MOBLIMA.action"},{"l":"MOBLIMA.configurables"},{"l":"MOBLIMA.configure"},{"l":"MOBLIMA.dataStructure"},{"l":"MOBLIMA.facade"},{"l":"MOBLIMA.handler"},{"l":"MOBLIMA.listing"},{"l":"MOBLIMA.panel"},{"l":"MOBLIMA.retrieval"},{"l":"MOBLIMA.save"},{"l":"MOBLIMA.utils"}];updateSearchResults();
